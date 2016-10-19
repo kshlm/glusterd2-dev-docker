cc6d0ad0a7b0cc84d5922532be9ae9993d6d77e7
Kaushal M <kshlmster@gmail.com>
2016-10-19 16:03:39 +0530

    Update setup script

--- gopath.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-export GOPATH=/go
-export PATH=$GOPATH/bin:/usr/local/go/bin:$PATH
-
--- setup.sh
+++ setup.sh
@@ -1,11 +1,42 @@
 #! /bin/bash
 
-yum install -y gdb tmux openssh-server sudo curl which gcc centos-release-gluster
-yum install -y glusterfs-server
-yum clean all
+# Install required stuff
+yum install -y gdb tmux curl which gcc git mercurial bzr subversion
 
-mkdir -p /go /usr/local
+# Setup Go
+if ! yum install -y 'golang >= 1.6'
+then
+  GOURL=https://storage.googleapis.com/golang
+  GOVERSION=1.6.3
+  GOARCHIVE=go${GOVERSION}.linux-amd64.tar.gz
+  mkdir /usr/local
+  curl -o /tmp/${GOARCHIVE} -L ${GOURL}/${GOARCHIVE}
+  tar -C /usr/local -xzf /tmp/${GOARCHIVE}
+  rm -f /tmp/${GOARCHIVE}
+fi
+
+# Setup GOPATH
+GOPATH=/go
+GOPATH_PROFILE=/etc/profile.d/gopath.sh
+mkdir -p /go
 chown vagrant: /go
+## Create $GOPATH_PROFILE to ensure GOPATH is setup for all users
+cat >${GOPATH_PROFILE} <<EOF
+#!/bin/sh
+export GOPATH=$GOPATH
+export PATH=\$GOPATH/bin:/usr/local/go/bin:\$PATH
+EOF
+chmod +x ${GOPATH_PROFILE}
 
-curl -o /tmp/golang.tar.gz https://storage.googleapis.com/golang/go1.6.3.linux-amd64.tar.gz
-tar -C /usr/local -xzf /tmp/golang.tar.gz
+#Install GlusterFS
+yum install -y glusterfs-server
+
+# Setup BASH prompt to show container IPs
+PROMPT_FILE=/etc/profile.d/0-prompt.sh
+cat >${PROMPT_FILE} <<'EOF'
+#!/bin/bash
+export PS1="[\u@\h($(hostname -i)) \W]\$ "
+EOF
+
+# Cleanup
+yum clean all
